c0f5c8e0de2f23eb0affa82dbeb2cc3a36167ad8
Alton Johnson <alton.jx@gmail.com>
2014-01-25 03:48:26 -0600

    Updated minor issue.

--- smbspider.py
+++ smbspider.py
@@ -256,7 +256,7 @@ class spider:
 							all_match = 0
 							break
 					if all_match == 1:
-						print "%s -c \"get \\\"%s\\\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
+						print "%s -c \"get \\\"%s\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
 						host, file_name, host, share, self.credentials)
 #						print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
 #						commands.getoutput("mv \"%s\" smbspider-downloads/%s" % (file_name, file_name))
