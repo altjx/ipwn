b4be22167d502dd8d9421060c7da13ae3ad54ba1
Alton Johnson <alton.jx@gmail.com>
2014-01-25 03:44:54 -0600

    Added auto file downloader to avoid grepping time.

--- smbspider.py
+++ smbspider.py
@@ -256,8 +256,8 @@ class spider:
 							all_match = 0
 							break
 					if all_match == 1:
-						result = commands.getoutput("%s -c \"get \\\"%s\\\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
-						host, file_name, host, share, self.credentials))
+						print "%s -c \"get \\\"%s\\\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
+						host, file_name, host, share, self.credentials)
 #						print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
 						commands.getoutput("mv \"%s\" smbspider-downloads/%s" % (file_name, file_name))
 		
