1777695fb2dd352cdb497393ae62bfbd3dba8214
Alton Johnson <alton.jx@gmail.com>
2014-01-25 03:56:51 -0600

    Updated minor issue.

--- smbspider.py
+++ smbspider.py
@@ -244,8 +244,8 @@ class spider:
 				if s in file_name:
 					result = commands.getoutput("%s -c \"get \\\"%s\\\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
 					host,file_name, host, share, self.credentials))
-#					print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
-#					commands.getoutput("mv \"%s\" smbspider-downloads/%s" % (file_name, file_name))
+					print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
+					commands.getoutput("mv \"%s_%s\" smbspider-downloads/%s" % (host, file_name, file_name))
 				else:
 					temp_file =  s.split("*")
 					all_match = 0
@@ -258,9 +258,8 @@ class spider:
 					if all_match == 1:
 						result = commands.getoutput("%s -c \"get \\\"%s\\\" \\\"%s_%s\\\"\" //%s/%s -U %s " %  (self.smbclient(), full_path.replace("\\","\\\\"), \
 						host,file_name, host, share, self.credentials))
-						print result
-#						print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
-						commands.getoutput("mv \"%s\" smbspider-downloads/%s" % (file_name, file_name))
+						print colors.blue + " [*] " + colors.norm + "Downloaded: %s from smb://%s/%s" % (file_name, host, share)
+						commands.getoutput("mv \"%s_%s\" smbspider-downloads/%s" % (host, file_name, file_name))
 		
 	def parse_result(self, result):
 		############################################################
