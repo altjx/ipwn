cf1822b78cf54aeb5a6911a265b076c1f37ba91a
Alton Johnson <alton.jx@gmail.com>
2014-02-20 15:44:58 -0600

    Updated reference in smb_spider.rb

--- msf-mods/smb_spider.rb
+++ msf-mods/smb_spider.rb
@@ -26,7 +26,7 @@ class Metasploit3 < Msf::Auxiliary
         ],
       'License'     => MSF_LICENSE,
       'References'   => [
-         ['URL','https://github.com/altjx/ipwn/blob/master/smb_spider.md']
+         ['URL','http://altonjx.wordpress.com/2014/02/20/finding-the-juicy-files-during-post-exploitation/']
       ]
     ))
 
