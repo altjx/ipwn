dd7a95b21f496902a2a91cc775a7c2930d4c6674
Alton Johnson <alton.jx@gmail.com>
2014-02-28 22:29:06 -0600

    Updated http_title.rb

--- msf-mods/http_title.rb
+++ msf-mods/http_title.rb
@@ -19,16 +19,16 @@ class Metasploit3 < Msf::Auxiliary
     super(
       'Name'        => 'HTTP Title Parser',
       'Description' => %q{Reports back titles from web page(s). Useful when
-		determining what's running on web servers. Extremely helpful when there's
-		tons of web servers.},
+      determining what's running on web servers. Extremely helpful when there's
+      tons of web servers.},
       'Author'      => 'Alton Johnson alton.jx[at]gmail.com',
       'License'     => MSF_LICENSE
     )
 
-		register_options([
-				OptInt.new('ReadBytes', [false, "Specify # of bytes to parse. 0 to parse entire pages", 0]),
-				OptBool.new('SSL', [false, "Negotiate SSL connection", false])
-			], self.class)
+      register_options([
+            OptInt.new('ReadBytes', [false, "Specify # of bytes to parse. 0 to parse entire pages", 0]),
+            OptBool.new('SSL', [false, "Negotiate SSL connection", false])
+         ], self.class)
 
     register_wmap_options({
         'OrderID' => 0,
@@ -40,45 +40,45 @@ class Metasploit3 < Msf::Auxiliary
     begin
       connect
 
-			opts = {'uri' => '/',
-			'method' => 'GET',
-			'SSL' => datastore['SSL']
-			}
+         opts = {'uri' => '/',
+         'method' => 'GET',
+         'SSL' => datastore['SSL']
+         }
 
       res = send_request_cgi!(opts)
       return if not res
 
-			if datastore['ReadBytes'] == 0
-				body = res.body.to_s
-			else
-				body = res.body.to_s[0..datastore['ReadBytes']]
-			end
+         if datastore['ReadBytes'] == 0
+            body = res.body.to_s
+         else
+            body = res.body.to_s[0..datastore['ReadBytes']]
+         end
 
-			# Parse web page response.
-			if (res.code >= 300 and res.code < 400) or body.include? "location.replace(\"https:"
-				print_error("#{ip}:#{rport} - 3xx or Java redirect")				
-			elsif res.code >= 200 and res.code < 300
-				print_title(ip, rport, body)
-			elsif res.code >= 300 and res.code < 400
-				print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")				
-			elsif res.code >= 400 and res.code < 500
-				print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")
-			elsif res.code >= 500 and res.code < 600
-				print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")
-			end
+         # Parse web page response.
+         if (res.code >= 300 and res.code < 400) or body.include? "location.replace(\"https:"
+            print_error("#{ip}:#{rport} - 3xx or Java redirect")
+         elsif res.code >= 200 and res.code < 300
+            print_title(ip, rport, body)
+         elsif res.code >= 300 and res.code < 400
+            print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")
+         elsif res.code >= 400 and res.code < 500
+            print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")
+         elsif res.code >= 500 and res.code < 600
+            print_error("#{ip}:#{rport} - <#{res.code} HTTP Response>")
+         end
 
-			disconnect
+         disconnect
     rescue ::Timeout::Error, ::Errno::EPIPE
     end
   end
 
-	def print_title(ip, rport, response)
-		title = response.to_s[/<title[^>]*>(.*?)<\/title>/,1]
-		if title.to_s.length == 0
-			 print_error("#{ip}:#{rport} - <No title found>")
-		else
-			 print_good("#{ip}:#{rport} - #{title}")
-		end
-	end
+   def print_title(ip, rport, response)
+      title = response.to_s[/<title[^>]*>(.*?)<\/title>/,1]
+      if title.to_s.length == 0
+          print_error("#{ip}:#{rport} - <No title found>")
+      else
+          print_good("#{ip}:#{rport} - #{title}")
+      end
+   end
 
 end
