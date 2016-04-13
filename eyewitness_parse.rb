b53e0d63ec76ff311bfa6ba356524992324fbab8
Alton Johnson <alton.jx@gmail.com>
2016-04-13 17:31:28 -0500

    EyeWitness parsing tool for CLI tools.

---
+++ eyewitness_parse.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+#
+# This tool simply parses the EyeWitness report in an HTML table for CLI tools
+# Run the script in the same directory that contains the reports. 
+#
+require 'nokogiri'
+require 'pry'
+require 'terminal-table'
+
+report_files = Dir.glob("./report*.html")
+
+table = Terminal::Table.new do |t|
+	t.headings = ['url','response code', 'title', 'powered by','server', 'report']
+	report_files.each do |report_name|
+		report = Nokogiri::HTML(File.open(report_name))
+		report.xpath('//td[contains(., "http://")]').each do |data|
+			data = data.to_s.split ("\n")
+			url = title = response = powered_by = server = ""
+			data.each do |d|
+				d = d.gsub("</b>", "").gsub("<br><b> ", "")
+				url = Nokogiri::HTML(d).xpath("//a").text if url.empty? and Nokogiri::HTML(d).xpath("//a").text.include? "http"
+				title = d.gsub("Page Title: ", "") if title.empty? and d.include? "Page Title: "
+				powered_by = d.gsub("x-powered-by: ", "") if powered_by.empty? and d.include? "x-powered-by: "
+				server = d.gsub("server: ", "") if server.empty? and d.include? "server: "
+				response = d.gsub("Response Code: ", "") if response.empty? and d.include? "Response Code: "
+			end
+			t.add_row [url, response, title[0..40], powered_by, server[0..20], report_name]
+		end
+		t.add_separator
+	end
+
+end
+
+puts table
