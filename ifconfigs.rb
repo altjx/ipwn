98a73fca0afe499a2334b0d34eff5773b9d5bd70
Alton Johnson <alton.jx@gmail.com>
2017-03-09 15:16:18 -0600

    Updated.

--- ifconfigs.rb
+++ ifconfigs.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'terminal-table'
 require 'ipaddr'
+require 'pry'
 
 ifconfig = `ifconfig`.split("\n\n")
 
@@ -18,23 +19,17 @@ table = Terminal::Table.new do |t|
   	int = ""
     # Regex used to grab for specific information.
     unless !int.empty?
-    	int = line.split(" ")[0]
+    	int = line.split(" ")[0][0..-2]
     end
-    ipaddr = line.scan(/addr:\b(?:\d{1,3}\.){3}\d{1,3}\b/)
-    netmask = line.scan(/Mask:\b(?:\d{1,3}\.){3}\d{1,3}\b/)
-    broadcast = line.scan(/Bcast:\b(?:\d{1,3}\.){3}\d{1,3}\b/)
-    mac = line.scan(/(?:[A-Fa-f0-9]{2}[:-]){5}(?:[A-Fa-f0-9]{2})/)
-
-    # Fix the variables if the regex matches aren't empty.
-    int = int unless int.nil?
-    ipaddr = ipaddr[0][5..-1] unless ipaddr.empty?
-    netmask = netmask[0][5..-1] unless netmask.empty?
-    broadcast = broadcast[0][6..-1] unless broadcast.empty?
-    omac = mac[0] unless mac.empty?
+    ipaddr = line.scan(/\b(?:\d{1,3}\.){3}\d{1,3}\b/)[0]
+    netmask = line.scan(/\b(?:\d{1,3}\.){3}\d{1,3}\b/)[1]
+    broadcast = line.scan(/\b(?:\d{1,3}\.){3}\d{1,3}\b/)[2]
+    mac = line.scan(/(?:[A-Fa-f0-9]{2}[:-]){5}(?:[A-Fa-f0-9]{2})/)[0]
 
     # Clean up some stuff
     oint = int unless int.empty?
-    broadcast = '' if broadcast.empty?
+    broadcast = '' if broadcast.nil?
+    omac = mac unless mac.nil?
     
     # Add the data to a table.
     unless oint.empty? and ipaddr.empty? and netmask.empty? and broadcast.empty? and omac.empty?
